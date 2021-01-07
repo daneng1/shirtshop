1a8223464dd6ba80e5fa83dfd73944aaa4d2ac13
daneng1 <engeldb@gmail.com>
2021-01-06 18:39:39 -0800

    added JS functions

--- app3.js
+++ app3.js
@@ -2,7 +2,8 @@ let today = new Date();
 let hourNow = today.getHours();
 let greeting;
 
-if (hourNow > 18) {
+
+if (hourNow > 17) {
     greeting = ' What a great evening to buy some tee\'s!';
 } else if (hourNow > 12) {
     greeting = ' What a great afternoon to buy some tee\'s!';
